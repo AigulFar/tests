e5fd6fc84a8729d1f9d70f0f06795f2d4d6e688b
AigulFar <92953212+AigulFar@users.noreply.github.com>
2025-07-09 00:29:40 +0300

    ci pipeline

--- unit/main_test.go
+++ unit/main_test.go
@@ -1,28 +1,7 @@
 package unit
 
-import (
-	"fmt"
-	"log"
+import "testing"
 
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/wait"
-)
-
-func main() {
-	ctx := context.Background()
-	req := testcontainers.ContainerRequest{
-		Image:      "hello-world",
-		WaitingFor: wait.ForLog("Hello from Docker!"),
-	}
-
-	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-		ContainerRequest: req,
-		Started:          true,
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer container.Terminate(ctx)
-
-	fmt.Println("Testcontainers works!")
+func TestMain(m *testing.M) {
+	// Пустой тест-заглушка
 }
